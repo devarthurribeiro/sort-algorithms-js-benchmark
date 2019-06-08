9d07fd9a3bb72a31a6ac1b98f4ce61b136bc9253
Arthur Ribeiro <devarthurribeiro@gmail.com>
2019-06-08 03:42:51 -0300

    move code to src/ path and rename vars

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-//gerador de numeros
+// gerador de numeros
 const generateNumbers = require('./generator')
 
-//importação dos algoritmos
+// importação dos algoritmos
 const quicksort = require('./quicksort')
 const insertionSort = require('./insertionSort')
 const mergesort = require('./mergesort')
 const bubbleSort = require('./bubblesort')
 const selectionsort = require('./selectionsort')
 
-//funções auxiliares
+// funções auxiliares
 const benchmark = require('./benchmark')
 const saveToFile = require('./saveToFile')
 const stf = d => JSON.stringify(d)
 
-//variação de numeros para o teste
+// variação de numeros para o teste
 const NUMBERS = ['10000', '100000', '1000000']
-//const NUMBERS = ['10', '50', '10']
+// const NUMBERS = ['10', '50', '10']
 
-//variação de ordenaçao
+// variação de ordenaçao
 const VARIANTS = ['asc', 'desc', 'random']
 
-//agrupamento de algoritmos
+// agrupamento de algoritmos
 const algorithms = {
   quicksort,
   insertionSort,
@@ -29,28 +29,29 @@ const algorithms = {
   selectionsort
 }
 
-//função para gerar numeros 
+// função para gerar numeros
 function setupNumbers () {
-  VARIANTS.forEach(mode => NUMBERS.forEach(max => generateNumbers(max, mode)))
+  VARIANTS.forEach(mode => NUMBERS.forEach(size => generateNumbers(size, mode)))
 }
 
-//função para rodar os testes
+// função para rodar os testes
 function runBenchmark () {
   const totalData = []
-  Object.keys(algorithms).forEach(key => {
+  Object.keys(algorithms).forEach(algorithm => {
     VARIANTS.forEach(mode =>
-      NUMBERS.forEach(max => {
+      NUMBERS.forEach(size => {
         for (let i = 0; i < 25; i++) {
-          let result = benchmark(algorithms[key], max, mode, key)
-          result.algorithm = key.toLowerCase()
+          let result = benchmark(algorithms[algorithm], size, mode, algorithm)
+          result.algorithm = algorithm.toLowerCase()
           result.mode = mode
-          result.size = max
+          result.size = size
           totalData.push(result)
         }
+        console.log('Finalizado', algorithm, mode, size)
       })
     )
   })
-  //salvando resultado dos testes
+  // salvando resultado dos testes
   saveToFile('./results/data.json', stf(totalData))
 }
 
